11903ce97349efbc442e8f52fcc8e9bd844217ec
niikuramasato <masakuraniito@gmail.com>
2019-09-02 12:06:18 +0900

    test

---
+++ testprogram/test.cpp
@@ -0,0 +1,18 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 2019/09/02
+ *      Author: masato
+ */
+
+#include "test.h"
+
+test::test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+test::~test() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ testprogram/test.h
@@ -0,0 +1,17 @@
+/*
+ * test.h
+ *
+ *  Created on: 2019/09/02
+ *      Author: masato
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class test {
+public:
+	test();
+	virtual ~test();
+};
+
+#endif /* TEST_H_ */
